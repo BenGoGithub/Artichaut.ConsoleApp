863c0fd5459085acf7c5559cad85e04f32fe4508
Benjamin Guenoun <benjamin.guenoun@snale.net>
2025-08-04 12:45:47 +0200

    Initial Commit

---
+++ Artichaut.ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Newtonsoft.Json;
+
+public class Meteo
+{
+    public string Ville { get; set; }
+    public float Temperature { get; set; }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        var data = new Meteo { Ville = "Paris", Temperature = 24.5f };
+        string json = JsonConvert.SerializeObject(data, Formatting.Indented);
+        Console.WriteLine(json);
+    }
+}
