ea8b8ab7965f45015d1cdd14b32084a5b88724da
Benjamin Guenoun <benjamin.guenoun@snale.net>
2025-08-05 15:47:06 +0200

    Add initial MSTest unit test project

---
+++ Artichaut.ConsoleApp.Tests/MeteoPeriodeTests.cs
@@ -0,0 +1,23 @@
+﻿using Artichaut.ConsoleApp.Domain;
+
+namespace Artichaut.ConsoleApp.Tests
+{
+    [TestClass]
+    public class MeteoPeriodeTests
+    {
+        [TestMethod]
+        public void Temperature2m_ShouldBeParsedCorrectly()
+        {
+            var json = @"{
+                ""temperature"": { ""2m"": 293.15 },
+                ""pression"": { ""niveau_de_la_mer"": 101325 }
+            }";
+
+            var period = Newtonsoft.Json.JsonConvert.DeserializeObject<MeteoPeriode>(json);
+
+            Assert.IsNotNull(period);
+            Assert.AreEqual(293.15f, period!.temperature!._2m);
+            Assert.AreEqual(101325, period.pression!.niveau_de_la_mer);
+        }
+    }
+}
